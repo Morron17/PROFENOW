a9ca13c52bd429c8ce063bc21f0cd730e850eb22
Morron17 <tomas.morrone@davinci.edu.ar>
2026-08-06 17:48:27 -0300

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,6 +7,7 @@
 <title>PROFENOW</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <script>
